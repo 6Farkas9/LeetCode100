419eeb95130f6967d83437d8a29a6eea8fe7fe35
6Farkas9 <dgszy00@163.com>
2025-03-08 18:03:22 +0800

    forth time start

--- 5_ordinary_array/t14/main.cpp
+++ 5_ordinary_array/t14/main.cpp
@@ -10,12 +10,12 @@ vector<vector<int>> intervals{{1,3},{2,6},{8,10},{15,18}};
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
-            if(a[0] == b[0])    return a[1] < b[1];
-            return a[0] < b[0];
-        });
+        int length = intervals.size();
+        
+        quickSort(intervals, 0, length - 1);
+
         vector<vector<int>> ans;
-        int length = intervals.size(), l_p = 0;
+        int l_p = 0;
         int start_n, end_n;
         while(l_p < length){
             start_n = intervals[l_p][0];
@@ -27,6 +27,33 @@ public:
             ans.push_back(vector<int>{start_n, end_n});
         }
         return ans;
+    }
+private:
+    void quickSort(vector<vector<int>> &intervals, int start, int end){
+        if(start >= end)    return;
+        int first = intervals[start][0];
+        int second = intervals[start][1];
+        int i = start, j = end;
+        while(i < j){
+            while(i < j && (intervals[j][0] > first || (intervals[j][0] == first && intervals[j][1] >= second))){
+                --j;
+            }
+            if(i >= j)  break;
+            intervals[i][0] = intervals[j][0];
+            intervals[i][1] = intervals[j][1];
+            ++i;
+            while(i < j && (intervals[i][0] < first || (intervals[i][0] == first && intervals[i][1] <= second))){
+                ++i;
+            }
+            if(i >= j)  break;
+            intervals[j][0] = intervals[i][0];
+            intervals[j][1] = intervals[i][1];
+            --j;
+        }
+        intervals[i][0] = first;
+        intervals[i][1] = second;
+        quickSort(intervals, start, i - 1);
+        quickSort(intervals, i + 1, end);
     }
 };
 
